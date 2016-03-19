9468290addbf906f7f2b5f069afcb791ef692e53
Montserrat Cristina Morató García <moratogarcia1987@gmail.com>
2016-03-18 19:42:07 -0600

    Iniciando

---
+++ redSocial/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^cuentas/',
+    	include('cuentas.urls',namespace="cuentas")),
+
+    url(r'^posts/',
+    	include('posts.urls',namespace="posts")),
+
+
+    
+
+    
+]
